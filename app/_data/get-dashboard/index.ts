d602e8a05b5bacf04eba2eceff1a668b58571330
Luciano <luciano.cupertino@hotmail.com>
2025-06-05 13:39:02 -0300

    feat: add expenses per category component

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -1,5 +1,6 @@
 import { db } from "@/app/_lib/prisma";
 import { TransactionType } from "@prisma/client";
+import { TotalExpensePerCategory } from "./types";
 
 export const getDashboard = async (month: string) => {
   const where = {
@@ -52,11 +53,30 @@ export const getDashboard = async (month: string) => {
       (Number(investimentsTotal || 0) / Number(transactionTotal)) * 100,
     ),
   };
+  const totalExpensePerCategory: TotalExpensePerCategory[] = (
+    await db.transaction.groupBy({
+      by: ["category"],
+      where: {
+        ...where,
+        type: TransactionType.EXPENSE,
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+  ).map((category) => ({
+    category: category.category,
+    totalAmount: Number(category._sum.amount),
+    percentageOfTotal: Math.round(
+      (Number(category._sum.amount) / Number(expensesTotal)) * 100,
+    ),
+  }));
   return {
     balance,
     depositsTotal,
     investimentsTotal,
     expensesTotal,
     typesPercentage,
+    totalExpensePerCategory,
   };
 };
